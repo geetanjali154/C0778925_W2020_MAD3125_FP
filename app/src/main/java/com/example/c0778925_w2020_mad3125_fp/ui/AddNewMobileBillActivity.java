beb1f50baef4a831d168ee95dbd53beda901c960
Geetanjali154 <geetanjaligupta154@gmail.com>
2020-04-20 06:21:58 +0530

    Mobile

--- app/src/main/java/com/example/c0778925_w2020_mad3125_fp/ui/AddNewMobileBillActivity.java
+++ app/src/main/java/com/example/c0778925_w2020_mad3125_fp/ui/AddNewMobileBillActivity.java
@@ -2,15 +2,20 @@ package com.example.c0778925_w2020_mad3125_fp.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.c0778925_w2020_mad3125_fp.R;
+import com.example.c0778925_w2020_mad3125_fp.models.Hydro;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -57,5 +62,49 @@ public class AddNewMobileBillActivity extends AppCompatActivity {
                 dialog.show();
             }
         });
+
+
+        btnSave1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(edtBillId1.getText().toString().isEmpty()){
+                    alertBox("Please enter Bill ID");
+                } else if(txtDate1.getText().toString().isEmpty()){
+                    alertBox("Please enter Bill Date");
+                } else if(edtPrice.getText().toString().isEmpty()){
+                    alertBox("Please enter Unit Price");
+                } else if(edtAgencyName.getText().toString().isEmpty()){
+                    alertBox("Please enter Agency Name");
+                } else if(edtUnit.getText().toString().isEmpty()){
+                    alertBox("Please enter Unit Consumed");
+                }
+                else
+
+                {
+                    //Hydro hydroo1 = new Hydro("HYDB0009", "16-Nov-2019", 9.00, "Planet Earth", 600);
+                    Hydro hydroo1 = new Hydro(edtBillId1.getText().toString(), txtDate1.getText().toString(), Double.parseDouble(edtPrice.getText().toString()), edtAgencyName.getText().toString(), Integer.parseInt(edtUnit.getText().toString()));
+                    c.addBill(hydroo1.getBillId(), hydroo1);
+                    Intent h = new Intent(AddNewHydroBillActivity.this, ShowBillDetailsActivity.class);
+                    startActivity(h);
+                }
+
+            }
+        });
+
     }
+    public void alertBox(String msg){
+        AlertDialog.Builder builder= new AlertDialog.Builder(AddNewHydroBillActivity.this);
+        builder.setTitle("Error");
+        //builder.setIcon(R.drawable.ic_action_plus);
+        builder.setMessage(msg);
+        builder.setCancelable(false);
+        builder.setPositiveButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(AddNewHydroBillActivity.this,"Cancel",Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.create().show();
+    }
+
 }
