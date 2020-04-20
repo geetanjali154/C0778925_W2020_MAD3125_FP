1c44f94c2fad99c233f197f50c8ccac706a5b4dc
Geetanjali154 <geetanjaligupta154@gmail.com>
2020-04-20 07:04:37 +0530

    bill

--- app/src/main/java/com/example/c0778925_w2020_mad3125_fp/models/Customer.java
+++ app/src/main/java/com/example/c0778925_w2020_mad3125_fp/models/Customer.java
@@ -82,20 +82,7 @@ public class Customer implements Serializable {
         this.bills = bills;
     }
 
-    public Double getTotalAmountToPay() {
-        return totalAmountToPay;
-    }
-
-    public void setTotalAmountToPay(Double totalAmountToPay) {
-        this.totalAmountToPay = totalAmountToPay;
-    }
-
-    private String calculateFullName(){
-        String fullName;
-        fullName = firstName + " " + lastName;
-        return fullName;
-    }
-
+  
 
 }
 
