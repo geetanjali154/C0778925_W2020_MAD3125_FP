dfe2c42c31fcac829675b924802c09b847f521f8
Geetanjali154 <geetanjaligupta154@gmail.com>
2020-04-20 05:31:17 +0530

    error removed

--- app/src/main/java/com/example/c0778925_w2020_mad3125_fp/ui/AddNewHydroBillActivity.java
+++ app/src/main/java/com/example/c0778925_w2020_mad3125_fp/ui/AddNewHydroBillActivity.java
@@ -37,7 +37,7 @@ public class AddNewHydroBillActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_hydro_bill);
-        
+        edtBillId1=(EditText)findViewById(R.id.edtBillId1);
         edtAgencyName=(EditText) findViewById(R.id.edtAgencyName);
         edtUnit=(EditText)findViewById(R.id.edtUnits);
         edtPrice=(EditText)findViewById(R.id.edtPrice);
