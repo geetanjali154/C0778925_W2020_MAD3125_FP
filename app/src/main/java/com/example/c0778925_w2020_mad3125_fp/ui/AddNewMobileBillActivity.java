17cf0347e9122e4d29a3a36001f7e52fbb3e750c
Geetanjali154 <geetanjaligupta154@gmail.com>
2020-04-20 07:14:48 +0530

    calender

--- app/src/main/java/com/example/c0778925_w2020_mad3125_fp/ui/AddNewMobileBillActivity.java
+++ app/src/main/java/com/example/c0778925_w2020_mad3125_fp/ui/AddNewMobileBillActivity.java
@@ -58,8 +58,7 @@ public class AddNewMobileBillActivity extends AppCompatActivity {
                     @Override
                     public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
 
-                        Calendar calendar = Calendar.getInstance();
-                        calendar.set(year, month, dayOfMonth);
+                       
                         Date date = calendar.getTime();
                         SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MMM-yyyy");
                         String s = dateFormat.format(date);
