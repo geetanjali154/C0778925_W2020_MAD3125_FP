69148ffca542a8a9821728e2c469b7ac42c59092
Geetanjali154 <geetanjaligupta154@gmail.com>
2020-04-20 07:10:31 +0530

    splash

--- app/src/main/java/com/example/c0778925_w2020_mad3125_fp/ui/SplashActivity.java
+++ app/src/main/java/com/example/c0778925_w2020_mad3125_fp/ui/SplashActivity.java
@@ -14,6 +14,16 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-    
+        new Handler().postDelayed(new Runnable() {
+
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                Intent i = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 5000);
     }
 }
