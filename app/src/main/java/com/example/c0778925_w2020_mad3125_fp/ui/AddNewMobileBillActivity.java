64a1b767c2cb9a2d339c92326e84e6903deb7591
Geetanjali154 <geetanjaligupta154@gmail.com>
2020-04-20 06:20:33 +0530

    Date

--- app/src/main/java/com/example/c0778925_w2020_mad3125_fp/ui/AddNewMobileBillActivity.java
+++ app/src/main/java/com/example/c0778925_w2020_mad3125_fp/ui/AddNewMobileBillActivity.java
@@ -2,12 +2,19 @@ package com.example.c0778925_w2020_mad3125_fp.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.c0778925_w2020_mad3125_fp.R;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class AddNewMobileBillActivity extends AppCompatActivity {
     private EditText edtBillId3;
@@ -32,5 +39,23 @@ public class AddNewMobileBillActivity extends AppCompatActivity {
         edtGbUsed=(EditText)findViewById(R.id.edtGbUsed);
         txtDate3=(TextView)findViewById(R.id.txtDate3);
         btnSave3=(Button)findViewById(R.id.btnSave3);
+        txtDate3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog dialog = new DatePickerDialog(AddNewHydroBillActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+
+                        Calendar calendar = Calendar.getInstance();
+                        calendar.set(year, month, dayOfMonth);
+                        Date date = calendar.getTime();
+                        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MMM-yyyy");
+                        String s = dateFormat.format(date);
+                        txtDate3.setText(s);
+                    }
+                }, Calendar.getInstance().get(Calendar.YEAR), Calendar.getInstance().get(Calendar.MONTH), Calendar.getInstance().get(Calendar.DAY_OF_MONTH));
+                dialog.show();
+            }
+        });
     }
 }
